c0dc3d1cdc58b2a4e72c3c07b61763dc8ed22188
Nicolas Morales <nicomorales297@gmail.com>
2024-11-06 23:43:49 -0300

    add functions testFunction

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/nmorales1991/hello-go
 
-go 1.20
+go 1.23.3
 
-require github.com/nmorales1991/greetings-go v0.0.0-20230726031758-1b1edad1f7f6
+require github.com/nmorales1991/greetings-go v0.0.0-20241107021508-26f60ecd1fce // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/nmorales1991/greetings-go"
+	// se agregó nuevo package al otro proyecto. Se hizo go get pero agregando un token de github para poder descargar el paquete, ya que es privado.
+	"github.com/nmorales1991/greetings-go/functions"
 	"github.com/nmorales1991/greetings-go/kuky"
 	"github.com/nmorales1991/hello-go/choco"
 	"log"
@@ -11,6 +13,7 @@ import (
 func main() {
 	kuky.HolaKuky()
 	choco.HolaChoco()
+	functions.TestFunction()
 
 	message, err := greetings.Hello("Nicolás")
 	if err != nil {
